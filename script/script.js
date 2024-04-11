0552904998619a4ad49ca4d22ee1dd9eed2f51a0
Saaamyak <samyakgoel5@gmail.com>
2024-04-11 21:55:15 +0530

    added sql

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const skillName = ['HTML-5','CSS-3','JavaScript','C++','Python','Flask','React Js','Mongo Db','SQL']
 const skillImg = ['/images/html5.png','/images/css-3.png','/images/Javascript.png','/images/cpp.png','/images/python.png','/images/flask.png','/images/react.png','/images/mongodb.png','/images/sql.png']
-const skillcol = ['#FFA500','#2196F3','#F7DF1E','#004481','#34709F','#eeeeee','#00D8FF','#1F6600','#1F6600']
+const skillcol = ['#FFA500','#2196F3','#F7DF1E','#004481','#34709F','#eeeeee','#00D8FF','#1F6600','#02A8F3']
 const skillLevel = ['ADVANCE','ADVANCE','INTERMEDIATE','INTERMEDIATE','Intermediate','BASIC','Intermediate','Basic','Basic']
 const skillVal = [90,90,75,75,85,90,80,70,70]
 $(document).ready(function(){
